b8171a7bc9f2361b775e5d547ea48ebd8374bb2a
Pavlo Bakhmach <geodezist@gmail.com>
2018-11-09 20:58:17 +0200

    [p-1] Create DB structure, install environment Developed scripts for creation of DB structure (PostgreSQL)

---
+++ src/main/resources/db/install-full.bat
@@ -0,0 +1,24 @@
+@echo off
+REM Copyright (c) 2018-2019 by Pavlo Bakhmach.  All rights reserved
+REM "Movie Land" full install from scratch scrit for PostgreSQL at Windows
+
+SET server=localhost
+SET /P server="Server [%server%]: "
+
+SET port=5432
+SET /P port="Port [%port%]: "
+
+SET username=postgres
+SET /P username="Username [%username%]: "
+
+SET path_to_psql=
+SET /P path_to_psql="Path to psql.exe [%path_to_psql%]: "
+
+
+for /f "delims=" %%a in ('chcp ^|find /c "932"') do @ SET CLIENTENCODING_JP=%%a
+if "%CLIENTENCODING_JP%"=="1" SET PGCLIENTENCODING=SJIS
+if "%CLIENTENCODING_JP%"=="1" SET /P PGCLIENTENCODING="Client Encoding [%PGCLIENTENCODING%]: "
+
+REM Run psql
+"%path_to_psql%\psql.exe" -h %server% -U %username% -p %port%  -a -f create_movieland_user.sql
+
