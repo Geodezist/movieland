f1aea1e4276fb6b3909f9efffa37f275d342a04f
Pavlo Bakhmach <geodezist@gmail.com>
2018-11-23 16:26:08 +0200

    Final commit  - changed static Map population in enums.

--- src/main/java/ua/com/bpgdev/movieland/common/SortingField.java
+++ src/main/java/ua/com/bpgdev/movieland/common/SortingField.java
@@ -1,34 +1,31 @@
 package ua.com.bpgdev.movieland.common;
 
-import java.util.HashMap;
 import java.util.Map;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public enum SortingField {
     PRICE("PRICE"),
     RATING("RATING");
 
     private String sortingField;
-    private static Map<String, SortingField> sortingFields = null;
+    private static Map<String, SortingField> sortingFields = Stream.of(values()).
+            collect(Collectors.toMap(SortingField::getSortingField, value -> value));
 
     SortingField(String sortingField) {
         this.sortingField = sortingField;
     }
 
     public static SortingField valueOfSortingField(String sortingField){
-        synchronized(SortingField.class) {
-            if (sortingFields == null) {
-                sortingFields = new HashMap<>();
-                for (SortingField value : values()) {
-                    sortingFields.put(value.sortingField, value);
-                }
-            }
-        }
-
         SortingField result = sortingFields.get(sortingField.toUpperCase().trim());
         if (result == null){
-            throw new IllegalArgumentException("");
+            throw new IllegalArgumentException("Invalid value \"" + sortingField + "\" for enum " +
+                    SortingField.class.getName());
         }
         return result;
     }
 
+    public String getSortingField() {
+        return sortingField;
+    }
 }
--- src/main/java/ua/com/bpgdev/movieland/common/SortingOrder.java
+++ src/main/java/ua/com/bpgdev/movieland/common/SortingOrder.java
@@ -2,32 +2,31 @@ package ua.com.bpgdev.movieland.common;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public enum SortingOrder {
     ASC("ASC"),
     DESC("DESC");
 
     private String sortingOrder;
-    private static Map<String, SortingOrder> sortingOrders = null;
+    private static Map<String, SortingOrder> sortingOrders = Stream.of(values()).
+            collect(Collectors.toMap(SortingOrder::getSortingOrder, value -> value));
 
     SortingOrder(String sortingOrder) {
         this.sortingOrder = sortingOrder;
     }
 
     public static SortingOrder valueOfSortingOrder(String sortingOrder){
-        synchronized(SortingOrder.class) {
-            if (sortingOrders == null) {
-                sortingOrders = new HashMap<>();
-                for (SortingOrder value : values()) {
-                    sortingOrders.put(value.sortingOrder, value);
-                }
-            }
-        }
-
         SortingOrder result = sortingOrders.get(sortingOrder.toUpperCase().trim());
         if (result == null){
-            throw new IllegalArgumentException("");
+            throw new IllegalArgumentException("Invalid value \"" + sortingOrder + "\" for enum " +
+                    SortingOrder.class.getName());
         }
         return result;
+    }
+
+    public String getSortingOrder() {
+        return sortingOrder;
     }
 }
