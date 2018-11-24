ce3cb6bdea8a6f5f7d1df276d9cbe33e399f6c57
Pavlo Bakhmach <geodezist@gmail.com>
2018-11-24 22:32:30 +0200

    [mm] Migrated Movieland to maven multi-module architecture.

---
+++ movieland-entity/src/main/java/ua/com/bpgdev/movieland/entity/Movie.java
@@ -0,0 +1,107 @@
+package ua.com.bpgdev.movieland.entity;
+
+import java.math.BigDecimal;
+import java.util.Objects;
+
+public class Movie {
+    private int id;
+    private String nameRussian;
+    private String nameNative;
+    private int yearOfRelease;
+    private double rating;
+    private BigDecimal price;
+    private String picturePath;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNameRussian() {
+        return nameRussian;
+    }
+
+    public void setNameRussian(String nameRussian) {
+        this.nameRussian = nameRussian;
+    }
+
+    public String getNameNative() {
+        return nameNative;
+    }
+
+    public void setNameNative(String nameNative) {
+        this.nameNative = nameNative;
+    }
+
+    public int getYearOfRelease() {
+        return yearOfRelease;
+    }
+
+    public void setYearOfRelease(int yearOfRelease) {
+        this.yearOfRelease = yearOfRelease;
+    }
+
+    public double getRating() {
+        return rating;
+    }
+
+    public void setRating(double rating) {
+        this.rating = rating;
+    }
+
+    public BigDecimal getPrice() {
+        return price;
+    }
+
+    public void setPrice(BigDecimal price) {
+        this.price = price;
+    }
+
+    public String getPicturePath() {
+        return picturePath;
+    }
+
+    public void setPicturePath(String picturePath) {
+        this.picturePath = picturePath;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null || getClass() != obj.getClass()) {
+            return false;
+        }
+        Movie movie = (Movie) obj;
+        return id == movie.id &&
+                nameRussian != null && nameRussian.equals(movie.nameRussian) &&
+                nameNative != null && nameNative.equals(movie.nameNative) &&
+                yearOfRelease == movie.yearOfRelease &&
+                rating == movie.rating &&
+                price != null && price.equals(movie.price) &&
+                picturePath != null && picturePath.equals(movie.picturePath)
+                ;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, nameRussian, nameNative, yearOfRelease, rating, price, picturePath);
+    }
+
+    @Override
+    public String toString() {
+        return "Movie{" +
+                "id=" + id +
+                ", nameRussian='" + nameRussian + '\'' +
+                ", nameNative='" + nameNative + '\'' +
+                ", yearOfRelease=" + yearOfRelease +
+                ", rating=" + rating +
+                ", price=" + price +
+                ", picturePath='" + picturePath + '\'' +
+                '}';
+    }
+}
---
+++ movieland-web/src/main/java/ua/com/bpgdev/movieland/web/controller/MovieController.java
@@ -0,0 +1,63 @@
+package ua.com.bpgdev.movieland.web.controller;
+
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.*;
+import ua.com.bpgdev.movieland.common.RequestParameters;
+import ua.com.bpgdev.movieland.common.SortingField;
+import ua.com.bpgdev.movieland.common.SortingOrder;
+import ua.com.bpgdev.movieland.common.SortingParameter;
+import ua.com.bpgdev.movieland.entity.Movie;
+import ua.com.bpgdev.movieland.service.MovieService;
+
+import java.util.List;
+
+@RestController
+public class MovieController {
+    private MovieService movieService;
+
+    public MovieController(@Qualifier("defaultMovieService") MovieService movieService) {
+        this.movieService = movieService;
+    }
+
+    @RequestMapping(path = "/movie", method = RequestMethod.GET, produces = {MediaType.APPLICATION_JSON_VALUE})
+    public List<Movie> getAll(@RequestParam(value = "rating", required = false) SortingOrder ratingSortingOrder,
+                              @RequestParam(value = "price", required = false) SortingOrder priceSortingOrder) {
+        if (ratingSortingOrder == null && priceSortingOrder == null) {
+            return movieService.getAll();
+        }
+        RequestParameters requestParameters = prepareRequestParameter(ratingSortingOrder, priceSortingOrder);
+        return movieService.getAll(requestParameters);
+    }
+
+    @RequestMapping(path = "/movie/random", method = RequestMethod.GET, produces = {MediaType.APPLICATION_JSON_VALUE})
+    public List<Movie> getThreeRandom(@RequestParam(value = "rating", required = false) SortingOrder ratingSortingOrder,
+                                      @RequestParam(value = "price", required = false) SortingOrder priceSortingOrder) {
+        if (ratingSortingOrder == null && priceSortingOrder == null) {
+            return movieService.getThreeRandom();
+        }
+        RequestParameters requestParameters = prepareRequestParameter(ratingSortingOrder, priceSortingOrder);
+        return movieService.getThreeRandom(requestParameters);
+    }
+
+    @RequestMapping(path = "/movie/genre/{genreId}", method = RequestMethod.GET, produces = {MediaType.APPLICATION_JSON_VALUE})
+    public List<Movie> getByGenreId(@PathVariable int genreId,
+                                    @RequestParam(value = "rating", required = false) SortingOrder ratingSortingOrder,
+                                    @RequestParam(value = "price", required = false) SortingOrder priceSortingOrder) {
+        if (ratingSortingOrder == null && priceSortingOrder == null) {
+            return movieService.getByGenreId(genreId);
+        }
+        RequestParameters requestParameters = prepareRequestParameter(ratingSortingOrder, priceSortingOrder);
+        return movieService.getByGenreId(genreId, requestParameters);
+    }
+
+    private RequestParameters prepareRequestParameter(SortingOrder ratingSortingOrder, SortingOrder priceSortingOrder) {
+        SortingParameter sortingParameter;
+        if (ratingSortingOrder != null) {
+            sortingParameter = new SortingParameter(SortingField.RATING, ratingSortingOrder);
+        } else {
+            sortingParameter = new SortingParameter(SortingField.PRICE, priceSortingOrder);
+        }
+        return new RequestParameters(sortingParameter);
+    }
+}
